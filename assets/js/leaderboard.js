5ccce706e6309ad3a9ca45a225ab4cc37a434e6d
flarsu <srishav669@gmail.com>
2020-03-26 17:04:12 +0530

    changes

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -12,27 +12,27 @@ var data = [
     {rank:"3",
     handleName:"Someone",
     Contribution:"325",
-    Year:"TE"
+    Year:"SE"
    },
    {rank:"4",
    handleName:"Someone",
    Contribution:"325",
-   Year:"TE"
+   Year:"FE"
   },
   {rank:"5",
   handleName:"Someone",
   Contribution:"325",
-  Year:"TE"
+  Year:"FE"
  },
  {rank:"6",
  handleName:"Someone",
  Contribution:"325",
- Year:"TE"
+ Year:"SE"
 }
 
 ];
 
-
+function all(data){
 var tblbdy = document.getElementById("tbdy");
  for(var index in data)
  {
@@ -49,4 +49,80 @@ var tblbdy = document.getElementById("tbdy");
      tblbdy.appendChild(tblrw);
  }   
 
+}
 
+function fes(data){
+   var tblbdy = document.getElementById("tbdy");
+    for(var index in data)
+    {
+       var tblrw = document.createElement('tr');
+        for(var info in data[index] )
+        {
+           var tbltd = document.createElement('td');
+           var tbldata = document.createTextNode(data[index][info]);
+           console.log(data[0][info]);
+           tbltd.appendChild(tbldata);
+           tblrw.appendChild(tbltd);
+        }
+        if( data[index]["Year"]=="FE")
+        tblbdy.appendChild(tblrw);
+    }   
+   
+   }
+   function ses(data){
+      var tblbdy = document.getElementById("tbdy");
+       for(var index in data)
+       {
+          var tblrw = document.createElement('tr');
+           for(var info in data[index] )
+           {
+              var tbltd = document.createElement('td');
+              var tbldata = document.createTextNode(data[index][info]);
+              console.log(data[0][info]);
+              tbltd.appendChild(tbldata);
+              tblrw.appendChild(tbltd);
+           }
+          if( data[index]["Year"]=="SE")
+           tblbdy.appendChild(tblrw);
+       }   
+      
+      }
+      function tes(data){
+         var tblbdy = document.getElementById("tbdy");
+          for(var index in data)
+          {
+             var tblrw = document.createElement('tr');
+              for(var info in data[index] )
+              {
+                 var tbltd = document.createElement('td');
+                 var tbldata = document.createTextNode(data[index][info]);
+                 console.log(data[0][info]);
+                 tbltd.appendChild(tbldata);
+                 tblrw.appendChild(tbltd);
+              }
+             if( data[index]["Year"]=="TE")
+              tblbdy.appendChild(tblrw);
+          }   
+         
+         }
+        
+function al()
+{
+   
+   all(data);
+}
+function fe()
+{
+
+   fes(data);
+}
+function se()
+{
+   
+   ses(data);
+}
+function te()
+{
+   
+   tes(data);
+}
